437bbf73dd2879aa3d8985ff45910661015dde41
Mehedi <mehedihasans807@gmail.com>
2022-11-27 22:10:23 +0600

    Final

--- test.cpp
+++ test.cpp
@@ -79,7 +79,7 @@ int main()
 		// system("clear");
 		cout << "\n  1.Add Employee Info.";
 		cout << "\n  2.Delete Employee Info.";
-		cout << "\n  3.Edit Employee Info";
+		cout << "\n  3.Modify Employee Info";
 		cout << "\n  4.Search Employee Info.";
 		cout << "\n  5.Display Employee Info. ";
 		cout << "\n  6.Exit Program. \n";
@@ -164,32 +164,70 @@ void delete_info()
 	file.close();
 	remove("date.dat");
 	rename("dtemp.dat", "data.dat");
+	cout<<" Info Deleted Successfully"<<endl;
 }
 
-// void edit_info()
-// {
-// 	int id;
-// 	cout << " Enter ID to modify : ";
-// 	cin >> id;
-
-// 	file.open("data.dat", ios::in | ios::out | ios::binary);
-// 		while(file.read((char *)&emp, sizeof(emp)));
-// 		{
-// 			if(emp.get_id() == id){
-// 				//string n;
-// 				int id;
-// 				cin>>id;
-// 				cout<<" Enter New Name : ";
-// 			//	cin>>n;
-// 				emp.update_id(id);    //update name
-// 				int pos = sizeof(emp);
-// 				file.seekp(-pos, ios::cur);
-// 				file.write((char *)&emp, sizeof(emp));
-// 			}
-
-// 		}
-// 		file.close();
-// }
+void edit_info()
+{
+	int id, flag = 0;
+	file.open("data.dat", ios::in | ios::binary | ios::out);
+	cout << "Enter Id to Modify : ";
+	cin >> id;
+	file.read((char *)&emp, sizeof(emp));
+	while (!file.eof())
+	{
+		if (id == emp.get_id())
+		{
+			flag = 1;
+			cout << " Edit Name? (Y/N): ";
+			char c;
+			cin >> c;
+			if (c == 'Y' or c == 'y')
+			{
+				string n;
+				cout << " Enter New Name : ";
+				cin >> ws;
+				cin >> n;
+				emp.update_name(n);
+				file.seekp(-sizeof(emp), ios::cur);
+				file.write((char *)&emp, sizeof(emp));
+			}
+			cout << " Edit Designation? (Y/N): ";
+			char ch;
+			cin >> ch;
+			if (ch == 'Y' or ch == 'y')
+			{
+				string n;
+				cout << " Enter New Designation : ";
+				cin >> ws;
+				cin >> n;
+				emp.update_des(n);
+				file.seekp(-sizeof(emp), ios::cur);
+				file.write((char *)&emp, sizeof(emp));
+			}
+			cout << " Edit Depertment? (Y/N): ";
+			char che;
+			cin >> che;
+			if (che == 'Y' or che == 'y')
+			{
+				string n;
+				cout << " Enter New Depertment : ";
+				cin >> ws;
+				cin >> n;
+				emp.update_dep(n);
+				file.seekp(-sizeof(emp), ios::cur);
+				file.write((char *)&emp, sizeof(emp));
+			}
+			break;
+		}
+		file.read((char *)&emp, sizeof(emp));
+	}
+	if (flag == 0)
+	{
+		cout << " Employee Info Not Found " << endl;
+	}
+	file.close();
+}
 void search_info()
 {
 	int id, flag = 0;
